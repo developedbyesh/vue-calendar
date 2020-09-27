ab9616f32a9613c2cd1148324683cc38ffee41ad
Imesh Gunasekara <imesh.gunasekara@serialberry.io>
2020-09-27 10:38:23 +0530

    firebase config and page set up

---
+++ src/components/Calendar.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-main>
+        <h1>My Calendar</h1>
+    </v-main>
+</template>
+
+<script>
+export default {
+    name: 'Calendar'
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify';
+import VueTextareaAutosize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+Vue.use(VueTextareaAutosize);
 
 Vue.config.productionTip = false
 
+firebase.initializeApp({
+  apiKey: "AIzaSyCJakPuSm8DeSDL6ARivv_xngV4Znxaln0",
+    authDomain: "vue-calendar-8c04b.firebaseapp.com",
+    databaseURL: "https://vue-calendar-8c04b.firebaseio.com",
+    projectId: "vue-calendar-8c04b",
+    storageBucket: "vue-calendar-8c04b.appspot.com",
+    messagingSenderId: "126086895212",
+    appId: "1:126086895212:web:e21268570e8dd0fbabea49"
+});
+
+export const db = firebase.firestore();
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
